3390bf7f4acfbf9a439394fd36aab83dfbced1aa
Jon Zhang <tangojon@gmail.com>
2022-03-16 17:18:01 -0400

    bootstrap update

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -4,4 +4,5 @@ define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/Controller/BaseController.php";
 require_once PROJECT_ROOT_PATH . "/Controller/FunctionController.php";
+require_once PROJECT_ROOT_PATH . "/Controller/GlobalVars.php";
 ?>
