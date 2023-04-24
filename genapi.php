c0645d519175588c8c3974d2a4025debd4d06844
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-04-24 14:39:59 -0400

    Manual loading of FunctionController

--- genapi.php
+++ genapi.php
@@ -4,6 +4,8 @@
  * See "/Controller/FunctionController.php" for Expected API endpoints
  */
 require __DIR__ . "/inc/bootstrap.php";
+require_once __DIR__ . "/Controller/FunctionController.php";
+
 
 $objFeedController = new FunctionController();
 $objFeedController->controller();
