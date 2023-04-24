0d36998b9f0523a4f995897087b8e03c1f466214
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-04-24 15:19:09 -0400

    remove unnecessary require

--- genapi.php
+++ genapi.php
@@ -6,8 +6,6 @@
 use NamespaceFunction\FunctionController;
 
 require __DIR__ . "/inc/bootstrap.php";
-require_once __DIR__ . "/Controller/FunctionController.php";
-
 
 $objFeedController = new FunctionController();
 $objFeedController->controller();
