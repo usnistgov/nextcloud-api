711b3347d19a427e8a7f71cd960a63ef683c7cfd
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-04-24 15:13:17 -0400

    update namespace

--- genapi.php
+++ genapi.php
@@ -3,6 +3,8 @@
  * Generic File Manager API entry point
  * See "/Controller/FunctionController.php" for Expected API endpoints
  */
+use NamespaceFunction\FunctionController;
+
 require __DIR__ . "/inc/bootstrap.php";
 require_once __DIR__ . "/Controller/FunctionController.php";
 
